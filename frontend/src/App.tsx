ab849063c6f247a93b628af0cb84c66a50bb9ea6
leoromero97 <leonardo.g.romero.dev@gmail.com>
2024-10-01 18:28:23 -0300

    setup project frontend with vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import ProductListApp from "./challenges/productList/App";
+
+function App() {
+  return <ProductListApp />;
+}
+
+export default App;
---
+++ frontend/src/challenges/productList/App.tsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+import api from "./api";
+import { Product } from "./types";
+
+function ProductListApp() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [query, setQuery] = useState<string>("");
+
+  useEffect(() => {
+    api.search(query);
+  }, [query]);
+
+  return (
+    <main>
+      <h1>Tienda digital</h1>
+      <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target)} />
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h4>{product.title}</h4>
+            <p>{product.description}</p>
+            <span>$ {product.price}</span>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export default ProductListApp;
